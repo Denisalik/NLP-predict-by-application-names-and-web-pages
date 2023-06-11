2ca684e0cf1c1850b322f963ceaa3e0e00e54385
Denisalik <schegletovdenis@mail.ru>
2023-06-12 00:31:41 +0300

    Example of classification

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import json
 
+from tabNamesCat import load_model, sentence_clean, get_mappings_to_category
+
 from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from model import SessionLocal, Activity, activity_to_dict
 
 app = FastAPI(title='app')
-
+vector_clf, clf = load_model()
+mappings = get_mappings_to_category()
 
 def get_session():
     db = SessionLocal()
@@ -26,7 +30,13 @@ def download(session: Session = Depends(get_session)):
     except:
         return False
 
-
-@app.get('/predict/{tab_name}')
-def predict(tab_name: str):
-    return tab_name
+class Tabs(BaseModel):
+    names: list[str]
+    # names = ["Купить Картридж лазерный Cactus CS-CE285AS (CE285A), черный, 1600 страниц, совместимый, для LJP M1132 / M1212nf / M1217nfw / P1102 / P1102w / P1214nfh / M1132s в Омске. Цены от интернет-магазина e2e4 - Google Chrome"]
+    # returns ["Utilities"]
+@app.post('/predict/')
+def predict(tabs: Tabs):
+    sentences = [sentence_clean(sentence) for sentence in tabs.names]
+    vectors = vector_clf.transform(sentences)
+    categories = clf.predict(vectors)
+    return [mappings[category] for category in categories]
